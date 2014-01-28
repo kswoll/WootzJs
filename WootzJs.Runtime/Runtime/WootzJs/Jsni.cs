af8e486f3470ba1d94e90eacecc873787e1dfe19
Kirk Woll <kirk.woll@gmail.com>
2014-01-28 15:55:47 -0800

    Changed jsni methods to use extern

--- WootzJs.Runtime/Runtime/WootzJs/Jsni.cs
+++ WootzJs.Runtime/Runtime/WootzJs/Jsni.cs
@@ -32,223 +32,65 @@ namespace System.Runtime.WootzJs
     [Js(Export = false)]
     public static class Jsni
     {
-        public static JsRegExp regex(string pattern, string suffix = null)
-        {
-            return null;
-        }
-
-        public static JsObject @new(JsObject target)
-        {
-            return null;
-        }
-
-        public static JsObject arguments()
-        {
-            return null;
-        }
-
-        public static JsObject @this()
-        {
-            return null;
-        }
-
-        public static JsObject apply(JsObject target, JsObject thisReference, JsArray arguments)
-        {
-            return null;
-        }
-
-        public static JsObject call(JsObject target, JsObject thisReference, params JsObject[] arguments)
-        {
-            return null;
-        }
-
-        public static JsObject apply<T>(Action<T> method, JsObject thisReference, JsArray arguments)
-        {
-            return null;
-        }
-
-        public static JsObject call<T>(Action<T> method, JsObject thisReference, params JsObject[] arguments)
-        {
-            return null;
-        }
-
-        public static void delete(JsObject o)
-        {
-        }
-
-        public static JsFunction procedure(Action a)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject> a, string nameOverride1 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject, JsObject> a, string nameOverride1 = null, string nameOverride2 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject, JsObject, JsObject> a,
-            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject> a,
-            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject> a,
-            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
+        public static extern JsRegExp regex(string pattern, string suffix = null);
+        public static extern JsObject @new(JsObject target);
+        public static extern JsObject arguments();
+        public static extern JsObject @this();
+        public static extern JsObject apply(JsObject target, JsObject thisReference, JsArray arguments);
+        public static extern JsObject call(JsObject target, JsObject thisReference, params JsObject[] arguments);
+        public static extern JsObject apply<T>(Action<T> method, JsObject thisReference, JsArray arguments);
+        public static extern JsObject call<T>(Action<T> method, JsObject thisReference, params JsObject[] arguments);
+        public static extern void delete(JsObject o);
+        public static extern JsFunction procedure(Action a);
+        public static extern JsFunction procedure(Action<JsObject> a, string nameOverride1 = null);
+        public static extern JsFunction procedure(Action<JsObject, JsObject> a, string nameOverride1 = null, string nameOverride2 = null);
+        public static extern JsFunction procedure(Action<JsObject, JsObject, JsObject> a,
+            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null);
+        public static extern JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null);
+        public static extern JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null);
+        public static extern JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
             string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null, 
-            string nameOverride6 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a, 
+            string nameOverride6 = null);
+        public static extern JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a, 
             string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null, 
-            string nameOverride6 = null, string nameOverride7 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride6 = null, string nameOverride7 = null);
+        public static extern JsFunction procedure(Action<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
             string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null, 
-            string nameOverride6 = null, string nameOverride7 = null, string nameOverride8 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject> a)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject> a,
-            string nameOverride1 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject, JsObject> a,
-            string nameOverride1 = null, string nameOverride2 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject, JsObject, JsObject> a,
-            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject> a,
-            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
-            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride6 = null, string nameOverride7 = null, string nameOverride8 = null);
+        public static extern JsFunction function(Func<JsObject> a);
+        public static extern JsFunction function(Func<JsObject, JsObject> a, string nameOverride1 = null);
+        public static extern JsFunction function(Func<JsObject, JsObject, JsObject> a,
+            string nameOverride1 = null, string nameOverride2 = null);
+        public static extern JsFunction function(Func<JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null);
+        public static extern JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null);
+        public static extern JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null);
+        public static extern JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
             string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null, 
-            string nameOverride6 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride6 = null);
+        public static extern JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
             string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null, 
-            string nameOverride6 = null, string nameOverride7 = null)
-        {
-            return null;
-        }
-
-        public static JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
+            string nameOverride6 = null, string nameOverride7 = null);
+        public static extern JsFunction function(Func<JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject, JsObject> a,
             string nameOverride1 = null, string nameOverride2 = null, string nameOverride3 = null, string nameOverride4 = null, string nameOverride5 = null, 
-            string nameOverride6 = null, string nameOverride7 = null, string nameOverride8 = null)
-        {
-            return null;
-        }
-
-        public static JsString _typeof(JsObject o)
-        {
-            return null;
-        }
-
-        public static JsObject member(JsObject target, string name)
-        {
-            return null;
-        }
-
-        public static JsObject memberset(JsObject target, string name, JsObject value)
-        {
-            return null;
-        }
-
-        public static JsArray array(params JsObject[] elements)
-        {
-            return null;
-        }
-
-        public static JsObject invoke(JsObject target, params JsObject[] arguments)
-        {
-            return null;
-        }
-
-        public static JsTypeFunction type<T>()
-        {
-            return null;
-        }
-
-        public static JsTypeFunction type(Type type)
-        {
-            return null;
-        }
-
-        public static JsObject reference(string identifier)
-        {
-            return null;
-        }
-
-        public static JsNumber parseInt(string s, int radix = 0)
-        {
-            return null;
-        }
-
-        public static JsNumber parseFloat(string s, int radix = 0)
-        {
-            return null;
-        }
-
-        public static bool isNaN(JsNumber number)
-        {
-            return false;
-        }
-
-        public static bool instanceof(JsObject expression, JsObject type)
-        {
-            return false;
-        }
-
-        public static JsObject @object(object o, bool compact = false)
-        {
-            return null;
-        }
+            string nameOverride6 = null, string nameOverride7 = null, string nameOverride8 = null);
+        public static extern JsString _typeof(JsObject o);
+        public static extern JsObject member(JsObject target, string name);
+        public static extern JsObject memberset(JsObject target, string name, JsObject value);
+        public static extern JsArray array(params JsObject[] elements);
+        public static extern JsObject invoke(JsObject target, params JsObject[] arguments);
+// ReSharper disable once UnusedTypeParameter
+        public static extern JsTypeFunction type<T>();
+        public static extern JsTypeFunction type(Type type);
+        public static extern JsObject reference(string identifier);
+        public static extern JsNumber parseInt(string s, int radix = 0);
+        public static extern JsNumber parseFloat(string s, int radix = 0);
+        public static extern bool isNaN(JsNumber number);
+        public static extern bool instanceof(JsObject expression, JsObject type);
+        public static extern JsObject @object(object o, bool compact = false);
     }
 }
