0898833672dae0245c048b3e8f3cf98fb0b33719
Kirk Woll <kirk.woll@gmail.com>
2014-02-06 18:36:54 -0800

    Update XmlHttpRequest

--- WootzJs.Web/XmlHttpRequest.cs
+++ WootzJs.Web/XmlHttpRequest.cs
@@ -6,6 +6,9 @@ namespace WootzJs.Web
     [Js(Name = "XMLHttpRequest", Export = false)]
     public class XmlHttpRequest
     {
+        [Js(Name = "response")]
+        public extern JsObject Response { get; }
+
         [Js(Name = "open")]
         public extern void Open(string method, string url, bool isAsync = true, string user = "", 
             string password = "");
@@ -18,5 +21,8 @@ namespace WootzJs.Web
 
         [Js(Name = "removeEventListener")]
         public extern void RemoveEventListener(string type, Action<Event> listener);
+
+        [Js(Name = "setRequestHeader")]
+        public extern void SetRequestHeader(string header, string value);
     }
 }
