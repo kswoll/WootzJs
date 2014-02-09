b73e8261932c6694760b442337b4a07b8486dd69
Kirk Woll <kirk.woll@gmail.com>
2014-02-09 11:19:23 -0800

    Add HTML control so you can add raw html to your layouts

---
+++ WootzJs.Mvc/Mvc/Views/Html.cs
@@ -0,0 +1,21 @@
+﻿using WootzJs.Web;
+
+namespace WootzJs.Mvc.Mvc.Views
+{
+    public class Html : Control
+    {
+        private string html;
+
+        public Html(string html)
+        {
+            this.html = html;
+        }
+
+        protected override Element CreateNode()
+        {
+            var result = Browser.Document.CreateElement("span");
+            result.InnerHtml = html;
+            return result;
+        }
+    }
+}
