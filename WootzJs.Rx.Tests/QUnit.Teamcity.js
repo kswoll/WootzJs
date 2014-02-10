133690b41296163b22e9dc6faed2559d6d27567d
Kirk Woll <kirk.woll@gmail.com>
2014-02-09 16:13:09 -0800

    Stub out unit test project for WootzJs.Rx

---
+++ WootzJs.Rx.Tests/QUnit.Teamcity.js
@@ -0,0 +1,24 @@
+﻿//QUnit.moduleStart({ name })
+QUnit.moduleStart = function (settings) {
+    console.log("##teamcity[testSuiteStarted name='" + settings.name + "']");
+};
+
+//QUnit.moduleDone({ name, failed, passed, total })
+QUnit.moduleDone = function (settings) {
+    console.log("##teamcity[testSuiteFinished name='" + settings.name + "']");
+};
+
+//QUnit.testStart({ name })
+QUnit.testStart = function (settings) {
+    console.log("##teamcity[testStarted name='" + settings.name + "']");
+};
+
+//QUnit.testDone({ name, failed, passed, total })
+QUnit.testDone = function (settings) {
+    if (settings.failed > 0) {
+        console.log("##teamcity[testFailed name='" + settings.name + "'"
+                     + " message='Assertions failed: " + settings.failed + "'"
+                     + " details='Assertions failed: " + settings.failed + "']");
+    }
+    console.log("##teamcity[testFinished name='" + settings.name + "']");
+};
