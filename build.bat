94a5ef733e70dbe601f15e5e4b09ad1fbafca454
Kirk Woll <kirk.woll@sqor.com>
2014-02-11 21:00:33 -0800

    Update build script

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe .\WootzJs.Compiler\WootzJs.Compiler.csproj
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe .\WootzJs.Runtime\WootzJs.Runtime.csproj
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe .\WootzJs.sln
