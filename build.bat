01e2eef0078b4154bb80560d07e2b6dc7556219e
Kirk Woll <kirk.woll@sqor.com>
2014-02-11 20:53:16 -0800

    Add build script

---
+++ build.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe .\WootzJs.Compiler\WootzJs.Compiler.csproj
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe .\WootzJs.Runtime\WootzJs.Runtime.csproj
