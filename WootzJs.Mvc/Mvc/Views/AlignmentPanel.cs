2899a451409626ad7ee013feca33ea7cbca4dea1
Kirk Woll <kirk.woll@gmail.com>
2014-02-17 17:09:55 -0800

    Re-implement alignment panel so that it honors containing width/height better (the panel's content now always takes up the entire space allocated to the AlignmentPanel)

--- WootzJs.Mvc/Mvc/Views/AlignmentPanel.cs
+++ WootzJs.Mvc/Mvc/Views/AlignmentPanel.cs
@@ -1,8 +1,13 @@
 ﻿
+using WootzJs.Web;
+
 namespace WootzJs.Mvc.Mvc.Views
 {
-    public class AlignmentPanel : TablePanel
+    public class AlignmentPanel : Control
     {
+        private Element cell;
+        private Element cellDiv;
+
         public static AlignmentPanel Top(Control content)
         {
             return new AlignmentPanel(content, HorizontalAlignment.Fill, VerticalAlignment.Top);
@@ -23,9 +28,64 @@ namespace WootzJs.Mvc.Mvc.Views
             return new AlignmentPanel(content, HorizontalAlignment.Left, VerticalAlignment.Fill);
         }
 
-        public AlignmentPanel(Control content, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment) : base(TableWidth.AllWeight(1))
+        public AlignmentPanel(Control content, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment) 
         {
-            Add(content, new TableConstraint(horizontalAlignment, verticalAlignment));
+            EnsureNodeExists();
+            switch (horizontalAlignment)
+            {
+                case HorizontalAlignment.Left:
+                    cell.SetAttribute("align", "left");
+                    break;
+                case HorizontalAlignment.Center:
+                    cell.SetAttribute("align", "center");
+                    break;
+                case HorizontalAlignment.Right:
+                    cell.SetAttribute("align", "right");
+                    break;
+                case HorizontalAlignment.Fill:
+                    cellDiv.Style.Width = "100%";
+                    cell.Style.Width = "100%";
+                    break;
+            }
+            switch (verticalAlignment)
+            {
+                case VerticalAlignment.Top:
+                    cell.Style.VerticalAlign = "top";
+                    break;
+                case VerticalAlignment.Middle:
+                    cell.Style.VerticalAlign = "middle";
+                    break;
+                case VerticalAlignment.Bottom:
+                    cell.Style.VerticalAlign = "bottom";
+                    break;
+                case VerticalAlignment.Fill:
+                    cell.Style.Height = "100%"; 
+                    cellDiv.Style.Height = "100%";
+                    break;
+            }
+
+            cellDiv.AppendChild(content.Node);
+            Add(content);
+        }
+
+        protected override Element CreateNode()
+        {
+            cellDiv = Browser.Document.CreateElement("div");
+
+            cell = Browser.Document.CreateElement("td");
+            cell.Style.Width = "100%";
+            cell.Style.Height = "100%";
+            cell.AppendChild(cellDiv);
+
+            var row = Browser.Document.CreateElement("tr");
+            var table = Browser.Document.CreateElement("table");
+            table.Style.Width = "100%";
+            table.Style.Height = "100%";
+
+            row.AppendChild(cell);
+            table.AppendChild(row);
+
+            return table;
         }
     }
 }
