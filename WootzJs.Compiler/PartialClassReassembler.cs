a3b7d8fbb6818af3884c55c8c262e699f3bc64bd
Kirk Woll <kirk.woll@gmail.com>
2014-12-07 09:55:00 -0800

    Remove spurious clone

--- WootzJs.Compiler/PartialClassReassembler.cs
+++ WootzJs.Compiler/PartialClassReassembler.cs
@@ -101,7 +101,7 @@ namespace WootzJs.Compiler
                 {
                     compilation = compilation.ReplaceSyntaxTree(item.Item1, SyntaxFactory.SyntaxTree(item.Item2, path: item.Item1.FilePath));                    
                 }
-                compilation = compilation.Clone();
+//                compilation = compilation.Clone();
                 Context.Update(project.Solution, project, compilation, new ReflectionCache(project, compilation), null);
             }
             return compilation;
