a0ed6ec07a909bb185f3775e104f84c537e4cdff
Kirk Woll <kirk.woll@gmail.com>
2014-12-13 14:09:02 -0800

    Add binders for Text control

---
+++ WootzJs.Mvc/Views/Binders/TextBinders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using WootzJs.Models;
+
+namespace WootzJs.Mvc.Views.Binders
+{
+    public static class TextBinders
+    {
+        public static void BindText<TModel, TValue>(this Bindings<TModel> bindings, Text text, Expression<Func<TModel, TValue>> property) where TModel : Model<TModel>
+        {
+            var model = bindings.Model;
+            var prop = model.GetProperty(property);
+
+            Action updateText = () => text.Value = (string)Convert.ChangeType(prop.Value, typeof(string));
+            updateText();
+
+            prop.Changed += updateText;
+            prop.Validated += validations => 
+            {
+                var application = text.ViewContext.ControllerContext.Application;
+                application.NotifyOnValidatedControl(text, validations);
+            };
+        }         
+    }
+}
