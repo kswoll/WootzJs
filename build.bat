91f445ad0ae0fe0e4e2d343d928f8488339eef14
MarkStega <mark@optimiumhealth.com>
2015-10-15 07:09:59 -0500

    2015-10-15 - Build changes

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 %MSBUILD% .\WootzJs.Compiler\WootzJs.Compiler.csproj
 %MSBUILD% .\WootzJs.Runtime\WootzJs.Runtime.csproj
-%MSBUILD% .\WootzJs.System\WootzJs.System.csproj
-%MSBUILD% .\WootzJs.ComponentModel.DataAnnotations\WootzJs.ComponentModel.DataAnnotations.csproj
-%MSBUILD% .\WootzJs.Compiler.Tests\WootzJs.Compiler.Tests.csproj
-rem %MSBUILD% .\WootzJs.Models.Net45\WootzJs.Models.Net45.csproj
-%MSBUILD% .\WootzJs.Web\WootzJs.Web.csproj
-%MSBUILD% .\WootzJs.Mvc\WootzJs.Mvc.csproj
-%MSBUILD% .\WootzJs.Rx\WootzJs.Rx.csproj
-%MSBUILD% .\WootzJs.Rx.Tests\WootzJs.Rx.Tests.csproj
-%MSBUILD% .\WootzJs.sln
+rem %MSBUILD% .\WootzJs.System\WootzJs.System.csproj
+rem %MSBUILD% .\WootzJs.ComponentModel.DataAnnotations\WootzJs.ComponentModel.DataAnnotations.csproj
+rem %MSBUILD% .\WootzJs.Compiler.Tests\WootzJs.Compiler.Tests.csproj
+rem%MSBUILD% .\WootzJs.Models\WootzJs.Models.csproj
+rem %MSBUILD% .\WootzJs.Web\WootzJs.Web.csproj
+rem %MSBUILD% .\WootzJs.Mvc\WootzJs.Mvc.csproj
+rem %MSBUILD% .\WootzJs.Rx\WootzJs.Rx.csproj
+rem %MSBUILD% .\WootzJs.Rx.Tests\WootzJs.Rx.Tests.csproj
+rem %MSBUILD% .\WootzJs.sln
