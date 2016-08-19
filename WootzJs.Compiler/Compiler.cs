f8d0f31aeb776d14d79b8a9b6a32fd4945c3c06a
Ray Hidayat <ray.hidayat@gmail.com>
2016-08-19 21:30:17 +1200

    Add MSBuild property by default so consumers can conditionally compile to both C# and WootzJs with the same csproj

--- WootzJs.Compiler/Compiler.cs
+++ WootzJs.Compiler/Compiler.cs
@@ -43,11 +43,13 @@ namespace WootzJs.Compiler
 
         private string mscorlib;
         private string[] defines;
+		private Dictionary<string, string> msbuildProperties;
 
-        public Compiler(string mscorlib, string[] defines)
+        public Compiler(string mscorlib, string[] defines, Dictionary<string, string> msbuildProperties)
         {
             this.mscorlib = mscorlib;
             this.defines = defines;
+			this.msbuildProperties = msbuildProperties;
         }
 
         public static void Main(string[] args)
@@ -70,6 +72,10 @@ namespace WootzJs.Compiler
             var define = namedArguments.Get("define");
             var defines = define == null ? new string[0] : define.Split(',');
 
+			var msbuildProperties = new Dictionary<string, string>() {
+				{  "WootzJs", "true" } // predefine reliable conditional property for all consumers
+			};
+
             if (performanceFile != null)
             {
                 Profiler.Output = new StreamWriter(performanceFile);
@@ -83,7 +89,7 @@ namespace WootzJs.Compiler
                 {
                     if (fileInfo.Extension.Equals(".sln", StringComparison.InvariantCultureIgnoreCase))
                     {
-                        var result = await Profiler.Time("Total Time", async () => await new Compiler(mscorlib, defines).CompileSolution(projectOrSolutionFile));                    
+                        var result = await Profiler.Time("Total Time", async () => await new Compiler(mscorlib, defines, msbuildProperties).CompileSolution(projectOrSolutionFile));                    
                         var output = result.Item1;
                         var solution = result.Item2;
                         var solutionName = fileInfo.Name.Substring(0, fileInfo.Name.Length - ".sln".Length);
@@ -91,7 +97,7 @@ namespace WootzJs.Compiler
                     }
                     else
                     {
-                        var result = await Profiler.Time("Total Time", async () => await new Compiler(mscorlib, defines).CompileProject(projectOrSolutionFile));
+                        var result = await Profiler.Time("Total Time", async () => await new Compiler(mscorlib, defines, msbuildProperties).CompileProject(projectOrSolutionFile));
                         var output = result.Item1;
                         var project = result.Item2;
                         var projectName = project.AssemblyName;
@@ -111,7 +117,7 @@ namespace WootzJs.Compiler
 
         public async Task<Tuple<string, Solution>> CompileSolution(string solutionFile)
         {
-//            var solution = await Profiler.Time("Loading Project", async () => await MSBuildWorkspace.Create().OpenSolutionAsync(solutionFile));
+//            var solution = await Profiler.Time("Loading Project", async () => await MSBuildWorkspace.Create(this.msbuildProperties).OpenSolutionAsync(solutionFile));
             var jsCompilationUnit = new JsCompilationUnit { UseStrict = true };
 
             // Since we have all the types required by the solution, we can keep track of which symbols are used and which
@@ -120,7 +126,7 @@ namespace WootzJs.Compiler
             RemoveUnusedSymbols = true;
 
             var projectFiles = FileUtils.GetProjectFiles(solutionFile);
-            var workspace = MSBuildWorkspace.Create();
+            var workspace = MSBuildWorkspace.Create(this.msbuildProperties);
             var solution = await Profiler.Time("Loading Solution", async () =>
             {
                 string mscorlib = this.mscorlib;
@@ -206,7 +212,7 @@ namespace WootzJs.Compiler
 //                File.SetLastWriteTime(projectUserFile, DateTime.Now);
 
             var jsCompilationUnit = new JsCompilationUnit { UseStrict = true };
-            var workspace = MSBuildWorkspace.Create();
+            var workspace = MSBuildWorkspace.Create(this.msbuildProperties);
             var project = await Profiler.Time("Loading Project", async () =>
             {
                 string mscorlib = this.mscorlib;
